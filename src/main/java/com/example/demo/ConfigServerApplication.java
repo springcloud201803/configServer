0fa9dfbe552f7c0ece981d695b6ec53cfffc4e61
painAdmin <1767123925@qq.com>
2018-03-12 14:40:07 +0800

    增加类注释

--- src/main/java/com/example/demo/ConfigServerApplication.java
+++ src/main/java/com/example/demo/ConfigServerApplication.java
@@ -2,8 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.config.server.EnableConfigServer;
+/**
+ * 分布式配置中心 ----服务端
+ * @author pain
+ *
+ */
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
